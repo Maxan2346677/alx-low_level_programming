64287c008f8fa2435df9598551c100dd7aff6ce2
maxan2346677 <maximax4990@gmail.com>
2023-10-24 09:53:38 -0700

    #include main.h

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,33 +1,19 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
-  * print_diagsums - main function
-  *
-  * @size: function parameter
-  *
-  * @a: function parameter
-  *
-  * Return: Always 0.
-  */
-
-void print_diagsums(int *a, int size)
+ * print_chessboard - Entry point
+ * @a: array
+ * Return: Always 0 (Success)
+ */
+void print_chessboard(char (*a)[8])
 {
-	int count1;
-	int count2;
-	int x;
+	int i;
+	int j;
 
-	count1 = 0;
-	count2 = 0;
-
-	for (x = 0; x < size; x++)
+	for (i = 0; i < 8; i++)
 	{
-	count1 = count1 + a[x * size + x];
+		for (j = 0; j < 8; j++)
+			_putchar(a[i][j]);
+		_putchar('\n');
 	}
-	for (x = size - 1; x >= 0; x--)
-	{
-	count2 += a[x * size + (size - x - 1)];
-	}
-	printf("%d, %d\n", count1, count2);
 }
 
